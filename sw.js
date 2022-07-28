0c6e256e3c08a270e13fc3e3a3fcf2f16791ca30
Luise Haller <luise@h4ll3r.net>
2022-07-27 17:19:20 -0700

    d

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./", "./src/styleNav.css", "./src/styleQuotes.css", "./images/logo192.png"]);
+            return cache.addAll(["./", "./src/styleNav.css"]);
         })
     );
 });
