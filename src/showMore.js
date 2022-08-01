c45df3c6d60981f3df2c8fe5103cbea262572a97
Luise Haller <luise@h4ll3r.net>
2022-08-01 15:37:06 -0700

    main

--- src/showMore.js
+++ src/showMore.js
@@ -1,27 +1,26 @@
-function toggleText () {
-    var points = document.getElementById("points");
-    var showMoreText = document.getElementById("moreText");
-    var buttonText = document.getElementById("showMoreTextButton");
+// function toggleText () {
+//     var points = document.getElementById("points");
+//     var showMoreText = document.getElementById("moreText");
+//     var buttonText = document.getElementById("showMoreTextButton");
 
-    if(points.style.display === "none") {
-        showMoreText.style.display = "none";
+//     if(points.style.display === "none") {
+//         showMoreText.style.display = "none";
 
-        points.style.display = "inline";
+//         points.style.display = "inline";
 
-        buttonText.innerHTML = "Show More";
+//         buttonText.innerHTML = "Show More";
 
-    }
-    else {
-        showMoreText.style.display = "inline";
+//     }
+//     else {
+//         showMoreText.style.display = "inline";
 
-        points.style.display = "none";
+//         points.style.display = "none";
 
-        buttonText.innerHTML = "Show Less";
-        var op = "0.2";
-        while (buttonText.innerHTML === "Show Less") {
-            buttonText.setAttribute('style', 'opacity:0.2;')
+//         buttonText.innerHTML = "Show Less";
+//         while (buttonText.innerHTML === "Show Less") {
+//             buttonText.setAttribute('style', 'opacity:0.2;')
             
-        }
+//         }
 
-    }
-}
+//     }
+// }
