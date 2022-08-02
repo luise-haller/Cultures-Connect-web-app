01f0654068544864f954c5056c51fddf3de787cf
Luise Haller <luise@h4ll3r.net>
2022-08-02 16:51:15 -0700

    d

--- src/moreButton.js
+++ src/moreButton.js
@@ -3,13 +3,19 @@ function toggleOpener() {
     var points = document.getElementById("points");
     var moreButton = document.getElementById("moreButton");
     var moreText = document.getElementById("moreText");
+
     
     if(points.style.display === "none") {
         moreText.style.display = "none";
 
         points.style.display = "inline";
 
+
         moreButton.innerHTML = "More";
+        if (moreButton.innerHTML === "More") {
+            moreButton.setAttribute('style', 'opacity: 0.8;')
+
+        }
 
     } else {
         moreText.style.display = "inline";
@@ -18,7 +24,8 @@ function toggleOpener() {
 
         moreButton.innerHTML = "Less";
         if (moreButton.innerHTML === "Less") {
-            buttonText.setAttribute('style', 'opacity:0.2;')
+            moreButton.setAttribute('style', 'opacity:0.5;')
+
         }
     }
 
